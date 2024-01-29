2f803fee37c7f421fb6b17a34f519456b04e5a9c
Samie-Tech <127851624+Samie-Tech@users.noreply.github.com>
2024-01-29 12:04:58 +0100

    Delete Portfolio directory

--- Portfolio/Javascript/cc.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburgerMenu.addEventListener('click', function () {
-        // Toggle the 'show' class on the navigation links
-        navLinks.classList.toggle('show');
-    });
-});
